f6f2670ec9d44d24cc9e097aae4b974886fec210
DONTASKME <bubblybaby069>
2025-10-13 09:27:00 +0530

    update file

--- mode.js
+++ mode.js
@@ -1,7 +1,33 @@
-function tooglemode(){
-            const body=document.getElementById('body');
-            const mode=document.getElementById('modelabel');
-            const isdark = body.classList.toggle('dark-mode');
-            mode.textContent=isdark ? 'Dark Mode' : 'Light Mode';
+function login() {
+      const username = document.getElementById("username").value.trim();
+      const password = document.getElementById("password").value.trim();
+      const error = document.getElementById("error");
 
-        }
+      if (username === "" || password === "") {
+        error.textContent = "⚠ Please enter both username and password!";
+      } else {
+        localStorage.setItem("username", username);
+        window.location.href = "admin.html";
+      }
+    }
+    const themeToggle = document.getElementById("themeToggle");
+    const body = document.body;
+
+    // Load saved theme
+    if (localStorage.getItem("theme") === "dark") {
+      body.classList.add("dark");
+      themeToggle.textContent = "☀️";
+    }
+
+    themeToggle.addEventListener("click", () => {
+      body.classList.toggle("dark");
+      const darkMode = body.classList.contains("dark");
+
+      if (darkMode) {
+        themeToggle.textContent = "☀️";
+        localStorage.setItem("theme", "dark");
+      } else {
+        themeToggle.textContent = "🌙 ";
+        localStorage.setItem("theme", "light");
+      }
+    });
